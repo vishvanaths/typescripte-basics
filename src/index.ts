c67bc50e2ceeeceda310a403d18b64c8573d4e62
Vishvanath Shinde <shinde.vishvanath@gmail.com>
2023-02-02 22:42:25 -0500

    Initial TS test

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+console.log('Hello World');
+
+let age : number = 20;
+
+if(age < 50)
+    age += 10;
+
+console.log(age)
+
+const enum Size {Small, Medium, Large};
+
+let mySize : Size = Size.Small;
